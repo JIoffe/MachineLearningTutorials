62d833df461a6fc9bc3161a1b322417f67aa7872
Jason Ioffe <Jason.Ioffe@insight.com>
2018-04-09 09:59:01 -0400

    Added bias and training to simple ann example

--- Neural Networks/simple_ann.py
+++ Neural Networks/simple_ann.py
@@ -13,29 +13,52 @@ import numpy as np
 X = np.array(([0,0],[1,1],[1,0],[0,1]), dtype=float)
 
 # our known y for these given features
-y = np.array(([0],[1],[0],[0]), dtype=float)
+y = np.array(([0],[1],[1],[1]), dtype=float)
 
 class NeuralNetwork(object):
     #Constructor
     def __init__(self):
         np.random.seed(2018)
-        self.w1 = np.random.randn(2,1)
+        self.layer0 = np.random.randn(2 + 1, 1)
         return
     
     def predict(self, X):
-        return self.__activation(np.dot(X, self.w1))
-    
+        X0 = self.__add_bias(X)
+        
+        return self.__activation(np.dot(X0, self.layer0))
+     
     def train(self, X, y, epochs = 100, learning_rate = 1):
-        for epoch in range(epochs):
-            output = self.__activation(np.dot(X, self.w1))
+        X0 = self.__add_bias(X)
+        
+        for epoch in range(epochs):             
+            output = self.__activation(np.dot(X0, self.layer0))
             
-            output_error = y - output
-            print(np.mean(np.abs(output_error)))
-            activation_error = output_error * self.__derive_activation(output)
-            weight_error = np.dot(X.T, activation_error)
+            if((epoch+1) % 100 == 0):
+                #using squared cost function
+                total_error = 0.5 * np.square(y - output)
+                average_total_error = np.mean(np.abs(total_error))
+                print('Epoch {}: Avg. Error = {}'.format(epoch+1, average_total_error))
+                
+            """
+                Follow the backpropagation:
+                     output error
+                     derivative of activation
+                   X input
+                   -------------------
+                     contribution of weight to error
+            """
             
-            self.w1 += learning_rate * weight_error
+            output_error = output - y
+            layer_0_delta = self.__derive_activation(output) * output_error
+            layer_0_adjustment = learning_rate * np.dot(X0.T, layer_0_delta)
+            
+            self.layer0 -= layer_0_adjustment
         return
+    
+    def __add_bias(self, X):
+        X_biased = np.ones((X.shape[0], X.shape[1] + 1))
+        X_biased[:, :-1] = X
+        return X_biased
     
     def __derive_activation(self, x):
         return x * (1 - x)                   # Sigmoid
@@ -44,3 +67,5 @@ class NeuralNetwork(object):
 
 neural_net = NeuralNetwork()
 neural_net.train(X, y, epochs = 1000, learning_rate = 1)
+
+prediction = neural_net.predict(X)
